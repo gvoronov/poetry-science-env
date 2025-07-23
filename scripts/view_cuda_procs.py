606d1c43292e65bcaa6980be5d33e833c7e707be
Gennady Voronov <gvoronov@gmail.com>
2025-07-23 15:59:49 -0600

    add scripts

--- scripts/view_cuda_procs.py
+++ scripts/view_cuda_procs.py
@@ -6,7 +6,6 @@ def main():
     parser.add_argument("--user", type=str, default="")
     parser.add_argument("--kill", action="store_true", default=False)
     args = parser.parse_args()
-    print(args)
 
     get_process_id_cmd = (
         f"sudo lsof /dev/nvidia* | awk '$3 == \"{args.user}\" {{print $2}}' | sort | uniq"
@@ -14,11 +13,11 @@ def main():
     out = subprocess.run(get_process_id_cmd, capture_output=True, shell=True)
     print(out.stdout.decode())
 
-    # if args.kill:
-    #     kill_cuda_procs_cmd = (
-    #         "kill -9 " + out.stdout.decode().rstrip("\n").replace("\n", " ")
-    #     )
-    #     _ = subprocess.run(kill_cuda_procs_cmd, shell=True)
+    if args.kill:
+        kill_cuda_procs_cmd = (
+            "kill -9 " + out.stdout.decode().rstrip("\n").replace("\n", " ")
+        )
+        _ = subprocess.run(kill_cuda_procs_cmd, shell=True)
 
 if __name__ == "__main__":
     main()
